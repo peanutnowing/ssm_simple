ef47e9dcbacf4cfe476e1c3304f0c676e0580e4f
peanutnowing <deble_king@126.com>
2019-01-21 17:29:03 +0800

    实体类：用户

---
+++ src/main/java/com/ssm/simple/entity/User.java
@@ -0,0 +1,40 @@
+package com.ssm.simple.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 实体类：用户
+ *
+ * @Author peanutnowing
+ * @Date 2019/1/21
+ */
+@Data
+public class User {
+    /**
+     * 主键
+     */
+    private Long id;
+    /**
+     * 用户名
+     */
+    private String userName;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 令牌
+     */
+    private String token;
+    /**
+     * 是否已经删除 0未删除 1已删除
+     */
+    private int isDeleted;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+}
+
