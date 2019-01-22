5bfdbc509a97c9bbffaedd4b495a77a86cef95d0
peanutnowing <deble_king@126.com>
2019-01-22 16:41:42 +0800

    用户相关实现类

---
+++ src/main/java/com/ssm/simple/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ssm.simple.service.impl;
+
+import com.ssm.simple.entity.User;
+import com.ssm.simple.service.UserService;
+
+/**
+ * 用户相关实现类
+ *
+ * @Author peanutnowing
+ * @Date 2019/1/22
+ */
+public class UserServiceImpl implements UserService {
+    /**
+     * 新增用户
+     * @param userName
+     * @param password
+     * @return
+     */
+    @Override
+    public int addUser(String userName, String password) {
+        return 0;
+    }
+    /**
+     * 登录
+     * @param userName
+     * @param password
+     * @return
+     */
+    @Override
+    public User loginByToken(String userName, String password) {
+        return null;
+    }
+    /**
+     * 根据token获取用户信息
+     * @param token
+     * @return
+     */
+    @Override
+    public User getUserByToken(String token) {
+        return null;
+    }
+}
