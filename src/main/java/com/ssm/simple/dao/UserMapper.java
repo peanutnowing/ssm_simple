0d65d20edf14e7d4e591321c2b30352a2efa2be5
peanutnowing <deble_king@126.com>
2019-01-24 18:00:00 +0800

    自动生成mapper

---
+++ src/main/java/com/ssm/simple/dao/UserMapper.java
@@ -0,0 +1,22 @@
+package com.ssm.simple.dao;
+
+import com.ssm.simple.entity.User;
+/**
+ * DAO:用户
+ *
+ * @Author peanutnowing
+ * @Date 2019/1/24
+ */
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
