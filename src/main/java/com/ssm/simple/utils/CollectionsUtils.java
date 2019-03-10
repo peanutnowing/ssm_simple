bd07631039a0603bea3daffaa54100e951e43370
peanutplan <peanutplan@qq.com>
2019-03-10 19:01:04 +0800

    List和Set互相转换,Set可以去重

---
+++ src/main/java/com/ssm/simple/utils/CollectionsUtils.java
@@ -0,0 +1,63 @@
+package com.ssm.simple.utils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static javafx.scene.input.KeyCode.T;
+
+/**
+ * 集合工具类
+ * Created by peanutnowing on 2019/3/10.
+ */
+public class CollectionsUtils {
+    /**
+     * Set转换为List
+     * @param set
+     * @return
+     */
+    public List<String> setToList(Set<String> set){
+        List<String> list = new ArrayList(new HashSet<>());
+        return list;
+    }
+
+    /**
+     * List转换为Set
+     * @param list
+     * @return
+     */
+    public Set<String> listToSet(List<String> list){
+        Set<String> set = new HashSet<>(list);
+        return set;
+    }
+
+
+    public static void main(String[] args) {
+       /* Set<String> set = new HashSet<>();
+        set.add("1");
+        set.add("2");
+        set.add("3");
+        for (String str:set){
+            System.out.println(str);
+        }
+        List<String> list = new ArrayList<>(set);
+        for (String str:list) {
+            System.out.println(str);
+        }*/
+        List<String> list = new ArrayList<>();
+        list.add("一");
+        list.add("二");
+        list.add("二");
+        list.add("三");
+        //List转换Set可以去重
+        Set<String> set = new HashSet<>(list);
+        for (String str:set
+             ) {
+            System.out.println(str);
+
+        }
+
+
+    }
+}
