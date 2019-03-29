cb2437371f8e20d889d2e441dba4b477a37960d7
peanutnowing <deble_king@126.com>
2019-03-29 17:53:02 +0800

    多线程demo

--- src/main/java/com/ssm/simple/demo/ThreadPoolDemo.java
+++ src/main/java/com/ssm/simple/demo/ThreadPoolDemo.java
@@ -1,14 +1,14 @@
-package com.ssm.simple.utils;
+package com.ssm.simple.demo;
 
 import java.util.concurrent.*;
 
 /**
- * 多线程工具类
+ * 多线程demo
  *
  * @Author peanutnowing
  * @Date 2019/3/18
  */
-public final class ThreadPoolUtils {
+public final class ThreadPoolDemo {
 
     private static final int queueSize = 50000;
 
@@ -28,4 +28,19 @@ public final class ThreadPoolUtils {
         executor.execute(command);
     }
 
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(2, 2, 0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(10),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
+
+        for (int i = 0; i < 20; i++) {
+            int index = i;
+            pool.submit(() -> System.out.println("i:" + index +
+                    " executorService"));
+        }
+        pool.shutdown();
+    }
 }
