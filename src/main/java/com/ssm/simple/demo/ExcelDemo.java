817f9493298621f3fd79fdcf8cf68e04da74f7d7
peanutnowing <deble_king@126.com>
2019-04-10 11:24:43 +0800

    POI操作Excel

---
+++ src/main/java/com/ssm/simple/demo/ExcelDemo.java
@@ -0,0 +1,54 @@
+package com.ssm.simple.demo;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * POI操作Excel
+ *
+ * @Author peanutnowing
+ * @Date 2019/4/9
+ */
+public class ExcelDemo {
+    public static void main(String[] args) throws IOException {
+        //map集合存储城市ID和城市名称
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"北京");
+        map.put(2,"上海");
+        map.put(3,"广州");
+        map.put(4,"深圳");
+        map.put(5,"杭州");
+
+        //1.创建文件
+        File file = new File("city.xls");
+        OutputStream outputStream = new FileOutputStream(file);
+        //2.创建工作簿及工作表
+        XSSFWorkbook  workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Sheet1");
+        //3.添加表头
+        XSSFRow row = sheet.createRow(0);
+        row.createCell(0).setCellValue("城市ID");
+        row.createCell(1).setCellValue("城市名称");
+        //4.添加内容——集合数据写入excel中
+        for(Map.Entry<Integer,String> entry: map.entrySet()){
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+            XSSFRow xssFRow = sheet.createRow(key);
+            xssFRow.createCell(0).setCellValue(key);
+            xssFRow.createCell(1).setCellValue(value);
+        }
+        //5.数据写入excel，释放资源
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
+
+    }
+}
