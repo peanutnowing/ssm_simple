0a40e5a36a57194dfac947414e8f04c8c961198c
peanutnowing <deble_king@126.com>
2019-04-19 20:18:40 +0800

    List基本用法

---
+++ src/main/java/com/ssm/simple/demo/ListDemo.java
@@ -0,0 +1,25 @@
+package com.ssm.simple.demo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * List基本用法
+ *
+ * @Author peanutnowing
+ * @Date 2019/4/19
+ */
+public class ListDemo {
+    public static void main(String[] args) {
+        List<Integer> numList = new ArrayList<>();
+        numList.add(2);
+        numList.add(55);
+        numList.add(66);
+        numList.add(88);
+        numList.add(77);
+
+        System.out.println(numList.get(0));
+        System.out.println(numList.get(numList.size()-1));
+     }
+}
