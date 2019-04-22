f7c0123e4df8aa6521d71998de071d55eeab8e22
peanutnowing <deble_king@126.com>
2019-04-22 14:00:07 +0800

    循环demo

---
+++ src/main/java/com/ssm/simple/demo/ForDemo.java
@@ -0,0 +1,19 @@
+package com.ssm.simple.demo;
+
+/**
+ * 循环demo
+ *
+ * @Author peanutnowing
+ * @Date 2019/4/22
+ */
+public class ForDemo {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4};
+        for (int i: array){
+            if(i%2==0) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
