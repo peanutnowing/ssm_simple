078686122c20c47b9675a0f5e4d0e308f5b71189
peanutnowing <deble_king@126.com>
2019-05-06 17:14:39 +0800

    字符串截取

--- src/main/java/com/ssm/simple/demo/StringDemo.java
+++ src/main/java/com/ssm/simple/demo/StringDemo.java
@@ -10,13 +10,23 @@ import com.ssm.simple.utils.StringSimpleUtils;
  */
 public class StringDemo {
     public static void main(String[] args) {
-        //去掉括号两种方法 1.截取截取字符串
+    /*    //去掉括号两种方法 1.截取截取字符串
         String numbers = "[1,2,3,4,5]";
         String[] array = StringSimpleUtils.removeBrackets(numbers);
         for (String str : array) {
             System.out.println(str);
         }
 
-        //2.正则表达式
+        //2.正则表达式*/
+
+        //熟悉String常用API，比如字符串截取等
+        String str ="莫斯科没有眼泪：@abcdefg美少女战士001:说得好(✪▽✪)";
+        System.out.println(str.substring(str.indexOf(":")+1,str.length()));
+     /*   String userAName ="小仙女0";
+        String userBName ="莫斯科";
+        String userNameA = userAName.length()>10?userAName.substring(0,10):userAName;
+        String nameGroup = userNameA+"、"+userBName;
+        String userNames=nameGroup.length()>10?nameGroup.substring(0,10)+"等":nameGroup+"等";
+        System.out.println(userNames);*/
     }
 }
