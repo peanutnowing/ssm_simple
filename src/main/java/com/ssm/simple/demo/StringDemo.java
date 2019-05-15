d7cd94c650f9fc58f181daf9ab5ef45430e2c765
peanutnowing <deble_king@126.com>
2019-05-15 11:13:42 +0800

    update

--- src/main/java/com/ssm/simple/demo/StringDemo.java
+++ src/main/java/com/ssm/simple/demo/StringDemo.java
@@ -1,6 +1,7 @@
 package com.ssm.simple.demo;
 
 import com.ssm.simple.utils.StringSimpleUtils;
+import org.junit.Test;
 
 /**
  * 字符串处理
@@ -10,32 +11,35 @@ import com.ssm.simple.utils.StringSimpleUtils;
  */
 public class StringDemo {
     /**
-     * 截取字符串
+     * 截取字符串方法1
      */
-    @org.junit.Test
-    public void subStringTest(){
-       /*    //去掉括号两种方法 1.截取截取字符串
+    @Test
+    public void subString1Test(){
         String numbers = "[1,2,3,4,5]";
         String[] array = StringSimpleUtils.removeBrackets(numbers);
         for (String str : array) {
-            System.out.                                                                   println(str);
-        }*/
+            System.out. println(str);
+        }
+    }
 
-        //熟悉String常用API，比如字符串截取等
-        String str ="莫斯科没有眼泪：@abcdefg美少女战士001:说得好(✪▽✪)";
-        System.out.println(str.substring(str.indexOf(":")+1,str.length()));
-     /*   String userAName ="小仙女0";
+    /**
+     * 截取字符串方法2
+     */
+    @Test
+    public void subString2Test() {
+        String str = "莫斯科没有眼泪：@abcdefg美少女战士001:说得好(✪▽✪)";
+        System.out.println(str.substring(str.indexOf(":") + 1, str.length()));
+        String userAName ="小仙女0";
         String userBName ="莫斯科";
         String userNameA = userAName.length()>10?userAName.substring(0,10):userAName;
         String nameGroup = userNameA+"、"+userBName;
         String userNames=nameGroup.length()>10?nameGroup.substring(0,10)+"等":nameGroup+"等";
-        System.out.println(userNames);*/
+        System.out.println(userNames);
     }
-
     /**
      * 字符串格式化
      */
-    @org.junit.Test
+    @Test
     public void formatStringTest(){
         String str ="<p><img src=\"%s\"></p><p><video class=\"edui-upload-video vjs-default-skin video-js\" controls=\"\" preload=\"none\" width=\"420\" height=\"280\" src=\"%s\" data-setup=\"{}\">\n" +
                 "<source src=\"%s\" type=\"video/mp4\"/></video></p>";
