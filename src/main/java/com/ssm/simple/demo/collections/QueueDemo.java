ea72a69d38e2a728d2d0710f0ad50cc45eb711d2
peanutplan <peanutplan@qq.com>
2019-05-16 21:59:33 +0800

    test

--- src/main/java/com/ssm/simple/demo/collections/QueueDemo.java
+++ src/main/java/com/ssm/simple/demo/collections/QueueDemo.java
@@ -1,7 +1,11 @@
 package com.ssm.simple.demo.collections;
 
+import org.junit.Test;
+
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 /**
  * 队列使用demo
@@ -27,5 +31,32 @@ public class QueueDemo {
             }
         }
 
+    }
+
+    /**
+     * 队列test
+     */
+    @Test
+    public void queueTest(){
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("www.111.com");
+        Set<String> urlSet = new HashSet<>();
+        boolean result = true;
+        while (result){
+            if(queue.isEmpty()){
+                result = false;
+            }else {
+                //移除并返问队列头部的元素，如果队列为空，则返回null
+                String url = queue.poll();
+                if (urlSet.contains(url)){
+                    break;
+                }else {
+                    urlSet.add(url);
+                }
+                System.out.println(url);
+                queue.offer("www.222.com");
+            }
+
+        }
     }
 }
