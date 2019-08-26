0e1bc5e561323e1a36174e8086ca630fd3e61d96
peanutnowing <deble_king@126.com>
2019-08-26 14:33:07 +0800

    stream分组用法

--- src/main/java/com/ssm/simple/demo/collections/StreamDemo.java
+++ src/main/java/com/ssm/simple/demo/collections/StreamDemo.java
@@ -1,9 +1,9 @@
 package com.ssm.simple.demo.collections;
 
+import com.ssm.simple.entity.User;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -13,12 +13,68 @@ import java.util.stream.Collectors;
  * @Date 2019/6/4
  */
 public class StreamDemo {
+    /**
+     *过滤
+     */
     @Test
-    public void streamTest(){
+    public void streamFilterTest(){
         List<String> names = Arrays.asList("张三", "李四", "王五", "张六");
         List<String> list = names.stream()
                 .filter(name -> name.startsWith("张"))
                 .collect(Collectors.toList());
         System.out.println(list);
+    }
+
+    /**
+     *分组
+     */
+    @Test
+    public void streamGroupTest(){
+        User user1 = new User();
+        user1.setId(1L);
+        user1.setUserName("张三");
+        user1.setAge(27);
+
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setUserName("李四");
+        user2.setAge(21);
+
+        User user3 = new User();
+        user3.setId(3L);
+        user3.setUserName("王五");
+        user3.setAge(29);
+
+        User user4 = new User();
+        user4.setId(4L);
+        user4.setUserName("刘六");
+        user4.setAge(38);
+
+        User user5 = new User();
+        user5.setId(1L);
+        user5.setUserName("张三");
+        user5.setAge(26);
+
+        List<User> userList = new ArrayList<>();
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        userList.add(user4);
+        userList.add(user5);
+        //过滤
+        userList = userList.stream().filter(t -> (t.getAge() < 30)).collect(Collectors.toList());
+        //根据年龄排序
+        Comparator<User> comparator = new Comparator<User>(){
+            @Override
+            public int compare(User u1, User u2) {
+                return u2.getAge()-u1.getAge();
+            }};
+        //分组
+        Map<Long, List<User>> userMap = userList.stream().collect(Collectors.groupingBy(user -> user.getId()));
+        for (Map.Entry<Long,  List<User>> entry : userMap.entrySet()) {
+            Collections.sort(entry.getValue(),comparator);
+            System.out.println("key= " + entry.getKey() + " and value= " + entry.getValue().get(0));
+        }
+
     }
 }
