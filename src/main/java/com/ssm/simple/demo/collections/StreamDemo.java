184e61c1b62d91925bb9a4eaf6ff66108b914c13
peanutnowing <deble_king@126.com>
2019-10-25 16:27:11 +0800

    update

--- src/main/java/com/ssm/simple/demo/collections/StreamDemo.java
+++ src/main/java/com/ssm/simple/demo/collections/StreamDemo.java
@@ -3,6 +3,12 @@ package com.ssm.simple.demo.collections;
 import com.ssm.simple.entity.User;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -18,12 +24,23 @@ public class StreamDemo {
      *过滤
      */
     @Test
-    public void streamFilterTest(){
+    public void streamFilterTest() throws ParseException {
         List<String> names = Arrays.asList("张三", "李四", "王五", "张六");
         List<String> list = names.stream()
                 .filter(name -> name.startsWith("张"))
                 .collect(Collectors.toList());
-        System.out.println(list);
+     //   System.out.println(list);
+
+     //   LocalDateTime localDateTime = LocalDateTime.parse("2019-07-31 00:00:00",dateTimeFormatter1);
+
+        DateTimeFormatter ymd = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        DateFormat df=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+       String yesterday =  ymd.format(LocalDate.now().minusDays(1));
+        long date1 = df.parse(yesterday+" 00:00:00" ).getTime()/1000;
+        long date2 = df.parse(yesterday+" 23:59:59" ).getTime()/1000;
+        System.out.println(date1);
+        System.out.println(date2);
     }
 
     /**
